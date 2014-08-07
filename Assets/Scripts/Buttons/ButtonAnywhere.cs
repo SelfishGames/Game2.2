7daddb0f469c8f72de601280b15d03ea61e72ee6
Rob <az5rob@gmail.com>
2014-08-07 15:25:19 +0100

    Added press anywhere button
    Dave coulda been Daquan wit a few kids

---
+++ Assets/Scripts/Buttons/ButtonAnywhere.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonAnywhere : MonoBehaviour
+{
+    #region Fields
+    public GameManager gameManager;
+    #endregion
+
+    void Update()
+    {
+        if (gameManager.isDead)
+        {
+
+
+        }
+    }
+
+    #region OnMouseDown
+    void OnMouseDown()
+    {
+       if (gameManager.isDead)
+        {
+            StartCoroutine("CallPlay");
+        }
+    }
+    #endregion
+
+    #region OnMouseUp
+    void OnMouseUp()
+    {
+
+    }
+    #endregion
+
+    IEnumerator CallPlay()
+    {
+        yield return new WaitForSeconds(.2f);
+        if (gameManager.playCount == 0)
+        {
+            gameManager.buttonManager.Play(1);
+        }
+        else
+        {
+            gameManager.buttonManager.Play(2);
+        }
+    }
+}
