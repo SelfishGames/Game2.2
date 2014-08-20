d5ed82e45ccd91184cf016b98071c83008466a3d
Rob <az5rob@gmail.com>
2014-08-20 14:53:06 +0100

    update gui text

---
+++ Assets/Scripts/GuiText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiText : MonoBehaviour
+{
+
+	public GUIText mainScore;
+	public GUIText[] endScores;
+
+    void Start ()
+    {
+    	// Control the size of the main score text. 
+    	mainScore.fontSize = Screen.width / 25;
+		// Control the size of the score that is shown when the player dies. 
+		for(int i = 0; i < 2; i++)
+		{
+			endScores[i].fontSize = Screen.width / 30;
+		}
+
+    }
+}
