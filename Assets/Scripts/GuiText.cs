a8c38b27d381b100156997bdfc14a4be971b22be
Rob <az5rob@gmail.com>
2014-08-20 15:17:51 +0100

    forgot the near miss

--- Assets/Scripts/GuiText.cs
+++ Assets/Scripts/GuiText.cs
@@ -4,18 +4,24 @@ using System.Collections;
 public class GuiText : MonoBehaviour
 {
 
-	public GUIText mainScore;
-	public GUIText[] endScores;
+    public GUIText mainScore;
+    public GUIText[] endScores;
+    public GUIText[] nearMiss;
 
     void Start ()
     {
-    	// Control the size of the main score text. 
-    	mainScore.fontSize = Screen.width / 25;
-		// Control the size of the score that is shown when the player dies. 
-		for(int i = 0; i < 2; i++)
-		{
-			endScores[i].fontSize = Screen.width / 30;
-		}
+        // Control the size of the main score text.
+        mainScore.fontSize = Screen.width / 25;
+        // Control the scale of the near miss text.
+        for (int i = 0; i < nearMiss.Length; i++)
+        {
+            nearMiss[i].fontSize = Screen.width / 25;
+        }
 
+        // Control the size of the score that is shown when the player dies.
+        for (int i = 0; i < endScores.Length; i++)
+        {
+            endScores[i].fontSize = Screen.width / 30;
+        }
     }
 }
