ed4959440fbe6b3666d9e3270f4fa92937519c57
Rob <az5rob@gmail.com>
2014-10-18 12:19:47 +0100

    Fuck my workout i gues

--- Assets/Scripts/Audio/TouchInput.cs
+++ Assets/Scripts/Audio/TouchInput.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 public class TouchInput : MonoBehaviour
 {
-
     #region Fields
     public LayerMask touchInputMask;
 
@@ -57,49 +56,6 @@ public class TouchInput : MonoBehaviour
                 }
             }
         }
-        //if (Input.touchCount > 0)
-        //{
-
-        //    oldTouch = new GameObject[touchList.Count];
-        //    touchList.CopyTo(oldTouch);
-        //    touchList.Clear();
-
-        //    foreach (Touch touch in Input.touches)
-        //    {
-        //        Ray ray = camera.ScreenPointToRay(touch.position);
-
-
-        //        if (Physics.Raycast(ray, out hit, touchInputMask))
-        //        {
-        //            GameObject recipient = hit.transform.gameObject;
-        //            touchList.Add(recipient);
-
-        //            if (touch.phase == TouchPhase.Began)
-        //            {
-        //                recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
-        //            }
-        //            if (touch.phase == TouchPhase.Ended)
-        //            {
-        //                recipient.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
-        //            }
-        //            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
-        //            {
-        //                recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
-        //            }
-        //            if (touch.phase == TouchPhase.Canceled)
-        //            {
-        //                recipient.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
-        //            }
-        //        }
-        //    }
-        //    foreach(GameObject g in oldTouch)
-        //    {
-        //        if(!touchList.Contains(g))
-        //        {
-        //            g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
-        //        }
-        //    }
-        //}
     }
     #endregion
 }
